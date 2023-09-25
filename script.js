8262082c93a376bee5a8c947682fb53ffa3c897b
abhay-59 <2021kuec2059@iiitkota.ac.in>
2023-09-25 23:16:43 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const n = 40;
 const array = [];
-let audioctx=null;
+let audioctx = null;
+let isSort = false;
 init();
 function init() {
   for (let i = 0; i < n; i++) {
@@ -9,34 +10,44 @@ function init() {
   show();
 }
 function play() {
-  const copy = [...array]; // this is call by refernce dynamic c++ '&'
-  const swaps = bubblesort(copy);
+  if (!isSort) {
+    isSort=true;
+    const copy = [...array]; // this is call by refernce dynamic c++ '&'
+    const swaps = bubblesort(copy);
     animate(swaps);
+  }
   //show();
 }
+function stop(){
+  isSort=false;
+}
 function animate(swaps) {
-//   if (swaps.length == 0) {
-//     show();
-//     return;
-//   }
-// for green line check at end
+  if(!isSort)
+  {
+    return;
+  }
+  //   if (swaps.length == 0) {
+  //     show();
+  //     return;
+  //   }
+  // for green line check at end
   if (swaps.length == 0) {
     show();
     const bars = document.querySelectorAll(".bar");
     for (let i = bars.length - 1; i >= 0; i--) {
       const bar = bars[i];
       const previousBar = bars[i + 1];
-      setTimeout(function() {
-          bar.style.backgroundColor = "green";
-          playnote(200 + array[i] * 500);
-          if (previousBar) {
+      setTimeout(function () {
+        bar.style.backgroundColor = "green";
+        playnote(200 + array[i] * 500);
+        if (previousBar) {
           previousBar.style.backgroundColor = "black";
-          playnote(200 + array[i+1] * 500)
-          if (i == 0){
+          playnote(200 + array[i + 1] * 500);
+          if (i == 0) {
             bars[0].style.backgroundColor = "black";
           }
-          ;}
-      }, 30 * (bars.length - i)); 
+        }
+      }, 30 * (bars.length - i));
     }
     return;
   }
@@ -44,7 +55,7 @@ function animate(swaps) {
   const [i, j] = swaps.shift();
   [array[i], array[j]] = [array[j], array[i]];
   show([j]);
-  playnote(200+array[i]*500);
+  playnote(200 + array[i] * 500);
   //playnote(200+array[j]*500);
   setTimeout(function () {
     animate(swaps);
@@ -70,29 +81,25 @@ function show(indices) {
     const bar = document.createElement("div");
     bar.style.height = array[i] * 100 + "%";
     bar.classList.add("bar");
-    if(indices && indices.includes(i))
-    {
-        bar.style.background="red";
+    if (indices && indices.includes(i)) {
+      bar.style.background = "red";
     }
     container.appendChild(bar);
   }
 }
-function playnote(freq)
-{
-    if(audioctx==null)
-    {
-        audioctx=new(
-            AudioContext
-            ||webkitAudioContext||window.webkitAudioContext
-        )();
-    }
-    const dur=0.1;
-    const osc=audioctx.createOscillator();
-    osc.frequency.value=freq;
-    osc.start();
-    osc.stop(audioctx.currentTime+dur);
-    const node=audioctx.createGain();
-    node.gain.value=0.05;
-    osc.connect(node);
-    node.connect(audioctx.destination);
+function playnote(freq) {
+  if (audioctx == null) {
+    audioctx = new (AudioContext ||
+      webkitAudioContext ||
+      window.webkitAudioContext)();
+  }
+  const dur = 0.1;
+  const osc = audioctx.createOscillator();
+  osc.frequency.value = freq;
+  osc.start();
+  osc.stop(audioctx.currentTime + dur);
+  const node = audioctx.createGain();
+  node.gain.value = 0.05;
+  osc.connect(node);
+  node.connect(audioctx.destination);
 }
